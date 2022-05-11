bca62fff59d78ac78d4918be6872acd69741bcfc
THOMAS HARDING <hardingt604@gmail.com>
2022-05-11 15:58:59 -0600

    add app.js

--- src/AuthPage.js
+++ src/AuthPage.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { signIn, signUp } from './services/fetch-utils.js';
 
 export default function AuthPage(props) {
-  // you'll need to track the form state of the email and password
+  // you'll need to track the form state of the email and password for sign in, and separate state for sign up
 
   async function handleSignIn(e) {
     e.preventDefault();
@@ -12,7 +12,8 @@ export default function AuthPage(props) {
     // set the user in App.js state using the correct prop callback. If you did the ternary right in App.js, this should automatically redirect the user to the board game list
   }
     
-  async function handleSignUp() {
+  async function handleSignUp(e) {
+    e.preventDefault()
     // sign the user up using the form state
 
     // set the user in App.js state using the correct prop callback. If you did the ternary right in App.js, this should automatically redirect the user to the board game list
@@ -21,7 +22,7 @@ export default function AuthPage(props) {
   return (
     <div className='auth'>
       <h1><em>Boardzo</em></h1>
-      {/* on submit, sign the user in using the function defined above */}
+      {/* on submit, sign the user up using the function defined above */}
       <form>
         <label>
             Email
@@ -33,9 +34,21 @@ export default function AuthPage(props) {
           {/* on change, update the form state for password */}
           <input required type="password" name="password" />
         </label>
+        <button>Sign Up</button>
+      </form>
+      {/* on submit, sign the user in using the function defined above */}
+     <form>
+        <label>
+            Email
+          {/* on change, update the form state for email */}
+          <input required type="email" name="email" />
+        </label>
+        <label>
+            Password
+          {/* on change, update the form state for password */}
+          <input required type="password" name="password" />
+        </label>
         <button>Sign In</button>
-        {/* on clicking sign up, sign the user up using the function defined above */}
-        <button type="button" >Sign Up</button>
       </form>
     </div>
   );
